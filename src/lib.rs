f8229485b4ca43f547d513a0c272ea862acf83bb
Andre Silva <andre.beat@gmail.com>
2016-06-06 00:57:33 +0100

    initial implementation of get, put and delete operations

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,21 @@ extern crate crc;
 extern crate time;
 
 use std::borrow::Cow;
-use std::io::{Cursor, Read, Write};
+use std::collections::HashMap;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use std::io::{Cursor, SeekFrom};
+use std::path::{Path, PathBuf};
 use std::vec::Vec;
 
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use crc::crc32;
+
+const ENTRY_STATIC_SIZE: usize = 14; // crc(4) + timestamp(4) + key_size(2) + value_size(4)
+const ENTRY_TOMBSTONE: u32 = !0;
+
+const DATA_FILE_EXTENSION: &'static str = "cask.data";
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Entry<'a> {
@@ -17,16 +27,13 @@ pub struct Entry<'a> {
     deleted: bool,
 }
 
-const STATIC_SIZE: usize = 14; // crc(4) + timestamp(4) + key_size(2) + value_size(4)
-const TOMBSTONE: u32 = !0;
-
 impl<'a> Entry<'a> {
     pub fn new<K, V>(key: K, value: V) -> Entry<'a>
         where Cow<'a, [u8]>: From<K>,
               Cow<'a, [u8]>: From<V>
     {
         let v = Cow::from(value);
-        assert!(v.len() < TOMBSTONE as usize);
+        assert!(v.len() < ENTRY_TOMBSTONE as usize);
 
         Entry {
             key: Cow::from(key),
@@ -36,27 +43,29 @@ impl<'a> Entry<'a> {
         }
     }
 
-    pub fn deleted(&self) -> Entry<'a> {
+    pub fn deleted<K>(key: K) -> Entry<'a>
+        where Cow<'a, [u8]>: From<K>
+    {
         Entry {
-            key: self.key.clone(),
+            key: Cow::from(key),
             value: Cow::Borrowed(&[]),
             timestamp: time::now().to_timespec().sec as u32,
             deleted: true,
         }
     }
 
-    pub fn size(&self) -> usize {
-        STATIC_SIZE + self.key.len() + self.value.len()
+    pub fn size(&self) -> u64 {
+        ENTRY_STATIC_SIZE as u64 + self.key.len() as u64 + self.value.len() as u64
     }
 
     pub fn to_bytes(&self) -> Vec<u8> {
-        let mut cursor = Cursor::new(Vec::with_capacity(self.size()));
+        let mut cursor = Cursor::new(Vec::with_capacity(self.size() as usize));
         cursor.set_position(4);
         cursor.write_u32::<LittleEndian>(self.timestamp).unwrap();
         cursor.write_u16::<LittleEndian>(self.key.len() as u16).unwrap();
 
         if self.deleted {
-            cursor.write_u32::<LittleEndian>(TOMBSTONE).unwrap();
+            cursor.write_u32::<LittleEndian>(ENTRY_TOMBSTONE).unwrap();
             cursor.write_all(&self.key).unwrap();
         } else {
             cursor.write_u32::<LittleEndian>(self.value.len() as u32).unwrap();
@@ -72,13 +81,13 @@ impl<'a> Entry<'a> {
     }
 
     pub fn write_bytes<W: Write>(&self, writer: &mut W) {
-        let mut cursor = Cursor::new(Vec::with_capacity(STATIC_SIZE));
+        let mut cursor = Cursor::new(Vec::with_capacity(ENTRY_STATIC_SIZE));
         cursor.set_position(4);
         cursor.write_u32::<LittleEndian>(self.timestamp).unwrap();
         cursor.write_u16::<LittleEndian>(self.key.len() as u16).unwrap();
 
         if self.deleted {
-            cursor.write_u32::<LittleEndian>(TOMBSTONE).unwrap();
+            cursor.write_u32::<LittleEndian>(ENTRY_TOMBSTONE).unwrap();
         } else {
             cursor.write_u32::<LittleEndian>(self.value.len() as u32).unwrap();
         }
@@ -116,15 +125,15 @@ impl<'a> Entry<'a> {
         let value_size = cursor.read_u32::<LittleEndian>().unwrap();
 
         Entry {
-            key: Cow::from(&bytes[STATIC_SIZE..STATIC_SIZE + key_size as usize]),
-            value: Cow::from(&bytes[STATIC_SIZE + key_size as usize..]),
+            key: Cow::from(&bytes[ENTRY_STATIC_SIZE..ENTRY_STATIC_SIZE + key_size as usize]),
+            value: Cow::from(&bytes[ENTRY_STATIC_SIZE + key_size as usize..]),
             timestamp: timestamp,
-            deleted: value_size == TOMBSTONE,
+            deleted: value_size == ENTRY_TOMBSTONE,
         }
     }
 
     pub fn from_read<R: Read>(reader: &mut R) -> Entry<'a> {
-        let mut header = vec![0u8; STATIC_SIZE as usize];
+        let mut header = vec![0u8; ENTRY_STATIC_SIZE as usize];
         reader.read(&mut header).unwrap();
 
         let mut cursor = Cursor::new(header);
@@ -136,7 +145,7 @@ impl<'a> Entry<'a> {
         let mut key = vec![0u8; key_size as usize];
         reader.read_exact(&mut key).unwrap();
 
-        let deleted = value_size == TOMBSTONE;
+        let deleted = value_size == ENTRY_TOMBSTONE;
 
         let value = if deleted {
             let empty: &[u8] = &[];
@@ -169,6 +178,142 @@ impl<'a> Entry<'a> {
     }
 }
 
+pub struct KeyEntry {
+    file_id: usize,
+    entry_pos: u64,
+    entry_size: u64,
+    timestamp: u32,
+}
+
+pub type KeyDir<'a> = HashMap<Cow<'a, [u8]>, KeyEntry>;
+
+fn get_data_file_path(path: &Path, file_id: usize) -> PathBuf {
+    path.join(file_id.to_string()).with_extension(DATA_FILE_EXTENSION)
+}
+
+fn get_file_handle(path: &Path, write: bool) -> File {
+    if write {
+        OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)
+            .unwrap()
+    } else {
+        OpenOptions::new()
+            .read(true)
+            .open(path)
+            .unwrap()
+    }
+}
+
+pub struct Cask<'a> {
+    path: &'a Path,
+    key_dir: KeyDir<'a>,
+    current_file_id: usize,
+    active_file: File,
+    sync: bool,
+}
+
+impl<'a> Cask<'a> {
+    pub fn open(path: &'a str, sync: bool) -> Cask<'a> {
+        let path = Path::new(path);
+
+        if path.exists() {
+            assert!(path.is_dir());
+        } else {
+            fs::create_dir(path).unwrap();
+        }
+
+        let mut key_dir: KeyDir<'a> = HashMap::new();
+
+        let current_file_id = 0;
+
+        let mut file = get_file_handle(&get_data_file_path(path, current_file_id), true);
+        let file_size = file.metadata().unwrap().len();
+
+        let mut file_pos = 0;
+        while file_pos < file_size {
+            let entry = Entry::from_read(&mut file);
+
+            if entry.deleted {
+                key_dir.remove(&entry.key);
+            }
+            else {
+                let key_entry = KeyEntry {
+                    file_id: current_file_id,
+                    entry_pos: file_pos,
+                    entry_size: entry.size(),
+                    timestamp: entry.timestamp,
+                };
+                key_dir.insert(entry.key, key_entry);
+            }
+
+            file_pos = file.seek(SeekFrom::Current(0)).unwrap();
+        }
+
+        Cask {
+            path: path,
+            key_dir: key_dir,
+            current_file_id: current_file_id,
+            active_file: file,
+            sync: sync,
+        }
+    }
+
+    pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        self.key_dir.get(&Cow::from(key)).and_then(|key_entry| {
+            let mut file = get_file_handle(&get_data_file_path(self.path, key_entry.file_id),
+                                           false);
+
+            file.seek(SeekFrom::Start(key_entry.entry_pos)).unwrap();
+
+            let mut entry = vec![0u8; key_entry.entry_size as usize];
+            file.read_exact(&mut entry).unwrap();
+
+            let entry = Entry::from_bytes(&entry);
+
+            if entry.deleted {
+                None
+            } else {
+                Some(entry.value.into_owned())
+            }
+        })
+    }
+
+    pub fn put(&mut self, key: &'a [u8], value: &[u8]) {
+        let entry = Entry::new(key, value);
+        let active_file_pos = self.active_file.seek(SeekFrom::Current(0)).unwrap();
+
+        entry.write_bytes(&mut self.active_file);
+
+        let key_entry = KeyEntry {
+            file_id: self.current_file_id,
+            entry_pos: active_file_pos,
+            entry_size: entry.size(),
+            timestamp: entry.timestamp,
+        };
+
+        self.key_dir.insert(Cow::from(key), key_entry);
+
+        if self.sync {
+            self.active_file.sync_data().unwrap();
+        }
+    }
+
+    pub fn delete(&mut self, key: &[u8]) {
+        if self.key_dir.remove(key).is_some() {
+            let entry = Entry::deleted(key);
+            entry.write_bytes(&mut self.active_file);
+
+            if self.sync {
+                self.active_file.sync_data().unwrap();
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::io::Cursor;
@@ -180,6 +325,7 @@ mod tests {
         let key: &[u8] = &[0, 0, 0];
         let value: &[u8] = &[0, 0, 0];
         let entry = Entry::new(key, value);
+        let deleted_entry = Entry::deleted(key);
 
         assert_eq!(entry.to_bytes().len(), 20);
 
@@ -189,43 +335,19 @@ mod tests {
         entry.write_bytes(&mut v);
         assert_eq!(entry, Entry::from_bytes(&v));
 
-        assert_eq!(entry.deleted(),
-                   Entry::from_bytes(&entry.deleted().to_bytes()));
-        assert_eq!(entry.deleted(),
-                   Entry::from_read(&mut Cursor::new(entry.deleted().to_bytes())));
+        assert_eq!(deleted_entry, Entry::from_bytes(&deleted_entry.to_bytes()));
+        assert_eq!(deleted_entry,
+                   Entry::from_read(&mut Cursor::new(deleted_entry.to_bytes())));
         v.clear();
-        entry.deleted().write_bytes(&mut v);
-        assert_eq!(entry.deleted(), Entry::from_bytes(&v));
-
-        let empty_entry = Entry::new(key, vec![]);
-        assert_eq!(empty_entry, Entry::from_bytes(&empty_entry.to_bytes()));
-        assert_eq!(empty_entry,
-                   Entry::from_read(&mut Cursor::new(&empty_entry.to_bytes())));
-        v.clear();
-        empty_entry.write_bytes(&mut v);
-        assert_eq!(empty_entry, Entry::from_bytes(&v));
-
-        assert_eq!(empty_entry.deleted(),
-                   Entry::from_bytes(&empty_entry.deleted().to_bytes()));
-        assert_eq!(empty_entry.deleted(),
-                   Entry::from_read(&mut Cursor::new(&empty_entry.deleted().to_bytes())));
-        v.clear();
-        empty_entry.deleted().write_bytes(&mut v);
-        assert_eq!(empty_entry.deleted(), Entry::from_bytes(&v));
-
-        assert!(Entry::from_bytes(&empty_entry.deleted().to_bytes()).deleted);
-        assert!(Entry::from_read(&mut Cursor::new(&empty_entry.deleted().to_bytes())).deleted);
-        assert!(Entry::from_bytes(&v).deleted);
-        assert!(empty_entry.deleted().deleted);
+        deleted_entry.write_bytes(&mut v);
+        assert_eq!(deleted_entry, Entry::from_bytes(&v));
     }
 
     #[test]
     fn test_deleted() {
         let key: &[u8] = &[0, 0, 0];
-        let value: &[u8] = &[0, 0, 0];
-        let entry = Entry::new(key, value);
 
-        assert!(entry.deleted().deleted);
-        assert_eq!(entry.deleted().value.len(), 0);
+        assert!(Entry::deleted(key).deleted);
+        assert_eq!(Entry::deleted(key).value.len(), 0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate cask;
+
+use cask::Cask;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cask = Cask::open("test.db", true);
+    cask.put("hello".as_bytes(), "world".as_bytes());
+    println!("{:?}", cask.get("hello".as_bytes()));
+    cask.delete("hello".as_bytes());
+    println!("{:?}", cask.get("hello".as_bytes()));
 }
