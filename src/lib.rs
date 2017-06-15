35bab63ea33dcf9462a1e388b665f0e1edf360ff
Andre Silva <andre.beat@gmail.com>
2017-06-15 01:02:23 +0100

    document cask module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! `Cask` is a key-value store backed by a log-structured hash table which is inspired by
+//! [bitcask](https://github.com/basho/bitcask/).
+//!
+//! Keys are indexed in a `HashMap` and values are written to an append-only log. To avoid the log
+//! from getting filled with stale data (updated/deleted entries) a compaction process runs in the
+//! background which rewrites the log by removing dead entries and merging log files.
+//!
+//! # Examples
+//!
+//! ```rust,no_run
+//! let cask = CaskOptions::default()
+//!     .compaction_check_frequency(1200)
+//!     .sync(SyncStrategy::Interval(5000))
+//!     .max_file_size(1024 * 1024 * 1024)
+//!     .open("cask.db")
+//!     .unwrap();
+//!
+//! let key = "hello";
+//! let value = "world";
+//!
+//! cask.put(key, value)?;
+//! cask.get(key)?;
+//! cask.delete(key)?;
+//! ```
+
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
