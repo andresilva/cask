0f1d666f33e2cc8211cdcd24587324be8c0ef475
cetra3 <cetra3@hotmail.com>
2018-03-03 11:43:24 +1030

    Adjust xxhash library to rust native

--- src/util.rs
+++ src/util.rs
@@ -4,27 +4,27 @@ use std::path::Path;
 use std::result::Result::Ok;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use xxhash2::{State32, hash32};
+use std::hash::Hasher;
 
-pub struct XxHash32(State32);
+use twox_hash::XxHash32;
 
-impl XxHash32 {
-    pub fn new() -> XxHash32 {
-        let mut state = State32::new();
-        state.reset(0);
-        XxHash32(state)
+pub struct TwoXhash32(XxHash32);
+
+impl TwoXhash32 {
+    pub fn new() -> TwoXhash32 {
+        TwoXhash32(XxHash32::with_seed(0))
     }
 
     pub fn update(&mut self, buf: &[u8]) {
-        self.0.update(buf);
+        self.0.write(buf);
     }
 
     pub fn get(&self) -> u32 {
-        self.0.finish()
+        self.0.finish() as u32
     }
 }
 
-impl Write for XxHash32 {
+impl Write for TwoXhash32 {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
         self.update(buf);
         Ok(buf.len())
@@ -36,7 +36,9 @@ impl Write for XxHash32 {
 }
 
 pub fn xxhash32(buf: &[u8]) -> u32 {
-    hash32(buf, 0)
+    let mut hash = XxHash32::with_seed(0);
+    hash.write(buf);
+    hash.finish() as u32
 }
 
 pub fn get_file_handle(path: &Path, write: bool) -> Result<File> {
